89d4545e7fa4cced8a84de4af1fd9272a5f88f4b
Gilberto <gilbertos.dev@gmail.com>
2025-05-18 21:30:19 -0300

    feat: add request and response helpers

---
+++ handler/request.go
@@ -0,0 +1,50 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequied(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
+
+// CreateOpening
+
+type CreateOpeningRequest struct {
+	Role      	string    `json:"role"`
+    Company   	string    `json:"company"`
+    Location  	string    `json:"location"`
+    Remote    	*bool     `json:"remote"`
+    Link      	string    `json:"link"`
+    Salary    	int64     `json:"salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil  && r.Link =="" && r.Salary <= 0{
+		return fmt.Errorf("request body is empty or malformed")
+	}
+
+	if r.Role == "" {
+		return errParamIsRequied("role", "string")
+	}
+
+	if r.Company == "" {
+		return errParamIsRequied("company", "string")
+	}
+
+	if r.Location == "" {
+		return errParamIsRequied("location", "string")
+	}
+
+	if r.Link == "" {
+		return errParamIsRequied("link", "string")
+	}
+
+	if r.Remote == nil{
+		return errParamIsRequied("remote", "bool")
+	}
+
+	if r.Salary <= 0 {
+		return errParamIsRequied("salary", "int64")
+	}
+
+	return nil
+}
